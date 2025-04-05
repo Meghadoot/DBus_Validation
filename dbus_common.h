c5ef5f90df07b44812b023b140fa5e1620243adc
Meghadoot <rkomeghadoot@gmail.com>
2025-04-05 03:51:23 +0200

    Add common config for  paths, interface, and service name macros

---
+++ dbus_common.h
@@ -0,0 +1,16 @@
+/*****************************
+
+Header for Dbus validation 
+
+******************************/
+
+#ifndef DBUS_COMMON_H
+#define DBUS_COMMON_H
+
+#define DBUS_SERVICE         "com.example.Middleware"
+#define DBUS_INTERFACE       "com.example.Middleware.Interface"
+#define DBUS_OBJECT_PATH     "/com/example/Middleware"
+#define DBUS_METHOD          "SendMessage"
+#define DBUS_SIGNAL          "Notify"
+
+#endif
